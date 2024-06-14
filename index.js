c2128f0cd3ebed9501e8beb379866da246af5c3b
Tarun Santhosh <tarun.santhosh@cavliwireless.com>
2024-06-15 02:04:01 +0530

    feat:  - index file created.  - node application created.

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const connectDB = require('./config/db');
+require('dotenv').config();
+const bucketRoutes = require('./routes/bucketRoutes');
+const fileRoutes = require('./routes/fileRoutes');
+
+const app = express();
+
+const port = process.env.PORT;
+
+//connect to database
+connectDB();
+
+//Middleware
+app.use(bodyParser.json());
+
+//Routes
+app.use('/api/buckets', bucketRoutes);
+app.use('/api/files', fileRoutes);
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+
+
+
