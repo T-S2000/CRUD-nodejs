f2c8d11d434fdf05fdd1672147b7a2c15c3f2831
tarun <tarunsan359@gmail.com>
2024-06-15 15:25:26 +0530

    fix:  - bucket Error handled.

--- controllers/bucketController.js
+++ controllers/bucketController.js
@@ -25,9 +25,12 @@ const deleteBucket = async (req, res) => {
     try {
         const files = await File.find({bucketId: req.params.bucketId});
         if(files.length > 0) return res.status(400).json({ message: 'Bucket contains files' });
-        await Bucket.findByIdAndDelete(req.params.bucketId);
-        
-        res.status(200).json({ message: 'Bucket deleted Successfully!' });
+        let bucket = await Bucket.findByIdAndDelete({_id: req.params.bucketId});
+        if(bucket == null){
+            return res.status(404).json({message: "Bucket not found!"});
+        }else{
+            return res.status(200).json({ message: 'Bucket deleted Successfully!' });
+        }
     } catch (error) {
         res.status(500).json({ error: 'Failed to delete bucket', error: error });
     }
